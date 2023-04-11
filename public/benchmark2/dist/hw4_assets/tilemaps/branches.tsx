c4a2f883b706e6fda1c75f0c58713e03ba02b432
Sean Yang <60932078+s303824@users.noreply.github.com>
2023-04-10 22:54:38 -0400

    better looking windows

--- public/benchmark2/dist/hw4_assets/tilemaps/branches.tsx
+++ public/benchmark2/dist/hw4_assets/tilemaps/branches.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="branches" tilewidth="16" tileheight="16" tilecount="270" columns="18">
- <image source="../../../../../../../branches.png" width="288" height="240"/>
+ <image source="branches.png" width="288" height="240"/>
 </tileset>
